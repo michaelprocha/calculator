4c953c1dec74e8cc906d985b55497d1391c8de81
Michael Rocha <michaelrocha9722@gmail.com>
2026-01-12 15:46:10 -0300

    finalize Calculator component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-
-function Calculator() {
+function Calculator({Children}) {
   return (
-    <div>Calculator</div>
+    <div className='bg-card w-89 h-141.5 px-8 pb-8 pt-13.5 flex'>
+      <div className='flex flex-col gap-6.5'>{Children}</div>
+    </div>
   )
 }
 
