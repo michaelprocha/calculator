cd424c0621eab1b7fc3e38f4f810900d13bde14f
Michael Rocha <michaelrocha9722@gmail.com>
2026-01-12 16:03:46 -0300

    fix card css component e Calculator component styles

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,6 @@
 function Calculator({Children}) {
   return (
-    <div className='card'>
+    <div className='card w-89 px-8 pb-8 pt-13.5 flex'>
       <div className='flex flex-col gap-6.5'>{Children}</div>
     </div>
   )
