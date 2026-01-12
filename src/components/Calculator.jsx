993e8b532a3ad29eb2b3b769a5c3e28db5b2f833
Michael Rocha <michaelrocha9722@gmail.com>
2026-01-12 15:59:49 -0300

    finalize card style

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,6 @@
 function Calculator({Children}) {
   return (
-    <div className='bg-card w-89 h-141.5 px-8 pb-8 pt-13.5 flex'>
+    <div className='card'>
       <div className='flex flex-col gap-6.5'>{Children}</div>
     </div>
   )
