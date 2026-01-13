d9aa916822c877b80f93a7fdd76533a70672ded3
Michael Rocha <michaelrocha9722@gmail.com>
2026-01-13 02:41:32 -0300

    adjustment equal sign

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,7 +4,7 @@ import Equals from '../assets/Equals.svg'
 function Result({children}) {
   return (
     <div className='px-4.5 flex justify-between'>
-      <img src={Equals} alt="Sinal de igual" />
+      <img className='w-1/10' src={Equals} alt="Sinal de igual" />
       <p className='uppercase text-text-result text-4xl'>{children}</p>
     </div>
   )
